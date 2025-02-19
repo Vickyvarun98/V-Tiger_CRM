8a1111172f8d8a030589fcf9945bb1224862602d
Vickyvarun98 <vigneshwaransesuragavan@gmail.com>
2025-02-19 10:30:47 +0530

    CRM is added....

---
+++ MavenPractice/src/test/java/Package1/NewclassTest.java
@@ -0,0 +1,35 @@
+package Package1;
+
+import org.testng.annotations.Test;
+
+public class NewclassTest {
+
+	@Test
+	public void test1() {
+		System.out.println("hii");
+		System.out.println("hello");
+		System.out.println("h r u");
+		System.out.println("wt r u doing");
+		System.out.println("had launch");
+
+		System.out.println("how is your days");
+
+		System.out.println("how is ur day");
+		System.out.println("exam ela rasavuuuuu");
+
+	}
+
+	@Test
+	public void test3() {
+		System.out.println("hii");
+		System.out.println("hello");
+
+		System.out.println("fine");
+
+		System.out.println("nothing");
+
+		System.out.println("yeah completed");
+
+	}
+
+}
---
+++ MavenPractice/src/test/java/Package1/PracticeTest.java
@@ -0,0 +1,27 @@
+package Package1;
+
+import org.testng.annotations.Test;
+
+public class PracticeTest {
+
+	@Test
+	public void Test3() {
+		// TODO Auto-generated method stub
+
+		for (int i = 0; i <= 5; i++) {
+			System.out.println(i);
+
+		}
+
+	}
+
+	@Test
+	public void Test4() {
+		// TODO Auto-generated method stub
+
+		for (int i = 0; i <= 5; i++) {
+			System.out.println("Hii");
+
+		}
+	}
+}
